e1c83c6bafefd3048246c756d1df59261b878f69
Marcelo Pitol <mrpitol@outlook.com>
2021-06-12 12:33:45 -0300

    Resolução Exercicio Drone

---
+++ src/ExercicioDrone.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ExercicioDrone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Um drone de limpeza de ruas deve escolher carregar sempre o lixo de menor peso. Entre os pesos de 3 lixos informados pelo usuário, um programa deve indicar qual deles deve ser carregado pelo drone.*/
+		//PRIMEIRA PARTE: PEDIR PARA O USUARIO DIGITAR OS DADOS
+		Scanner leitor = new Scanner(System.in);
+		double lixo1, lixo2, lixo3;
+		System.out.println("digite o peso do primeiro lixo:");
+		lixo1 = leitor.nextDouble();
+		System.out.println("digite o peso do segundo lixo:");
+		lixo2 = leitor.nextDouble();
+		System.out.println("digite o peso do terceiro lixo:");
+		lixo3 = leitor.nextDouble();
+				
+		//SEGUNDA PARTE: DESCOBRIR QUAL É O LIXO DE MENOR PESO
+		// Para o primeiro lixo ser o menor, o que eu preciso fazer?
+		//Ele precisa ser ao mesmo tempo, menor que o lixo 2 e menor que o lixo 3
+		if ((lixo1<lixo2)&&(lixo1<lixo3)) {
+			System.out.println("Carregar Lixo1");
+		}else if((lixo2<lixo3)&&(lixo2<lixo1)) {
+				System.out.println("Carregar Lixo2");
+		}else { if((lixo3<lixo1)&&(lixo3<lixo2)) 
+				System.out.println("Carregar Lixo3");
+		}
+		leitor.close();
+		}
+	}
+
+
---
+++ src/ExercicioTriangulo.java
@@ -0,0 +1,13 @@
+
+public class ExercicioTriangulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Crie um programa que peça que o usuário digite os valores dos 3 lados de um triângulo.
+O seu programa deverá validar se o triângulo existe. (A regra para isso é que a soma de quaisquer 2 lados do triângulo tem que ser SEMPRE maior do que o terceiro).
+Além disso, caso o triângulo exista, o seu programa deverá mostrar o tipo dele.*/
+		
+
+	}
+
+}
