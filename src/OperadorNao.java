70ea48d6502f1233e6fb07c90614e0e1628d360f
Marcelo Pitol <mrpitol@outlook.com>
2021-06-12 11:18:18 -0300

    Implementado o operador Not

--- src/OperadorNao.java
+++ src/OperadorNao.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class OperadorNao {
 
 	public static void main(String[] args) {
-		/*Crie um programa que permita que receba o tipo de um usuário em uma variável String e caso esse tipo NÃO SEJA "administrador", exibir a mensagem: "Atenção, usuário sem poderes administrativos. Algumas tarefas serão bloqueadas".*/
+		/*Crie um programa que receba o tipo de um usuário em uma variável String e caso esse tipo NÃO SEJA "administrador", exibir a mensagem: "Atenção, usuário sem poderes administrativos. Algumas tarefas serão bloqueadas".*/
 		Scanner leitor = new Scanner(System.in);
 		String tipoUsuario;
 		System.out.println("Por favor, digite seu tipo de usuário:");
 		tipoUsuario = leitor.next();
 		
 		//Aqui é preciso criar um if que verifique se o tipo de usuário digitado é DIFERENTE de "administrador"
+		if (!tipoUsuario.equalsIgnoreCase("administrador")) {
+			System.out.println("Atenção, usuário sem poderes administrativos. Algumas tarefas serão bloqueadas");
+		}else {
+			System.out.println("Usuário com poderes administrativos. Nenhuma tarefa será bloqueadas");
+		}
 		
 		leitor.close();
 		
